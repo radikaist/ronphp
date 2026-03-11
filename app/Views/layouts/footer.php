586c0fe53cb4348cb8957c5dd515f213e745e9f3
Radika Ist <11767310+radikaist@users.noreply.github.com>
2026-03-11 08:07:40 +0700

    Commit

--- app/Views/layouts/footer.php
+++ app/Views/layouts/footer.php
@@ -1,6 +1,6 @@
 </main>
             <footer style="padding: 20px 30px; text-align: left; color: #94a3b8; font-size: 13px; border-top: 1px solid #e2e8f0; background: #fff;">
-                &copy; <?= date('Y'); ?> <strong>RON PHP Framework</strong>. Dibuat oleh Radika Istiawan.
+                &copy; <?= date('Y'); ?> <strong>RON (Radika Origin Native) PHP Framework</strong>. Dibuat oleh Radika Istiawan.
             </footer>
 
         </div> 
