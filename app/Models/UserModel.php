e2f760b7ac0823a90996cc35a750e35bf41ed1ad
Radika Ist <11767310+radikaist@users.noreply.github.com>
2026-03-11 07:19:10 +0700

    commit

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-use App\Core\Database; // Panggil mesin database
+use App\Core\Database;
 
 class UserModel
 {
@@ -10,14 +10,21 @@ class UserModel
 
     public function __construct()
     {
-        // Hubungkan model ini dengan mesin database PDO
         $this->db = new Database();
     }
 
-    // Fungsi untuk mengambil semua data dari tabel users
+    // Fungsi untuk mengambil SEMUA data pengguna (Untuk Beranda)
     public function getAllUsers()
     {
         $this->db->query('SELECT * FROM users');
-        return $this->db->resultSet(); // Ambil datanya sebagai Array
+        return $this->db->resultSet();
+    }
+
+    // FUNGSI BARU: Mengambil SATU data pengguna berdasarkan ID
+    public function getUserById($id)
+    {
+        $this->db->query('SELECT * FROM users WHERE id = :id');
+        $this->db->bind(':id', $id); // Mengikat ID untuk mencegah SQL Injection
+        return $this->db->single(); // Gunakan single() karena hanya 1 baris data
     }
 }
